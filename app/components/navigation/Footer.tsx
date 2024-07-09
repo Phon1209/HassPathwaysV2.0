8ce490c9261c6ec0e8f25f5954ad8592c0ff9c83
Evan Whymark <whymarkevan@gmail.com>
2024-07-09 14:52:55 -0400

    Added Search Courses

--- app/components/navigation/Footer.tsx
+++ app/components/navigation/Footer.tsx
@@ -3,21 +3,23 @@ import { SiDiscord, SiGithub } from "react-icons/si";
 import Link from "next/link";
 
 const Footer = () => {
-  return (
-    <footer className="footer">
-      <div className="flex flex-row sm:flex-col md:flex-row gap-4">
-        <SiDiscord className="icon" />
-        <SiGithub className="icon" />
-      </div>
-      <div className="flex flex-row sm:flex-col md:flex-row gap-4 sm:gap-x-12">
-        <Link href="/faq">FAQ</Link>
-        <Link href="/courses/search">Search Courses</Link>
-        <Link href="/courses">My Courses</Link>
-        <Link href="/pathways">My Pathways</Link>
-      </div>
-      <div>&copy; 2023 Hass Pathways</div>
-    </footer>
-  );
+    return (
+        <footer className="footer">
+            <div className="flex grid-cols-2 gap-4">
+                <SiDiscord className="icon"/>
+                <SiGithub className="icon"/>
+            </div>
+            <div
+                className="flex justify-center space-x-12 mx-auto text-md ">
+                <Link href="/faq">FAQ</Link>
+                <Link href="/courses">My Courses</Link>
+                <Link href="/pathways">My Pathways</Link>
+                <Link href="/courses/search">Search Courses</Link>
+            </div>
+        </footer>
+    );
+
 };
+
 
 export default Footer;

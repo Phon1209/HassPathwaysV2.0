753f268706ae22f9f5ec680fe89515359ea70252
noobzhuz <75611029+noobzhuz@users.noreply.github.com>
2024-07-12 15:32:36 -0400

    update dropdown css

--- app/components/navigation/CatalogDropdown.tsx
+++ app/components/navigation/CatalogDropdown.tsx
@@ -10,11 +10,20 @@ const CatalogDropdown = () => {
   const { catalog_year, setCatalog } = useAppContext();
   const [dropdownOpen, setDropdownOpen] = useState(false);
   const dropdownRef = useRef<HTMLDivElement>(null);
-  const [newCatalogText, setCatalogText] = useState("2022-2023");
+  //const [newCatalogText, setCatalogText] = useState("");
+  const [newCatalogTextFirst, setFirst] = useState("");
+  const [newCatalogTextSecond, setSecond] = useState("");
+
+  // Find the displaying text for catalog
+  const catalogText: string =
+    catalogList.filter((cat) => cat.text === catalog_year)[0]?.text ?? "";
 
   useEffect(()=> {
-    setCatalogText(catalog_year)
-  }, [catalog_year]);
+    const temp = catalogList.filter((cat) => cat.text === catalog_year)[0]?.text ?? "";
+    var str = temp.split("-");
+    setFirst(str[0]);
+    setSecond(str[1]);
+  });
 
   useEffect(() => {    
     if (!dropdownOpen) return;
@@ -33,13 +42,13 @@ const CatalogDropdown = () => {
 
   return (
     <div
-      className="dropdown cursor-pointer flex items-center gap-2 w-[200px] md:w-[320px] bg-white border-solid border border-gray-300 text-gray-500 py-[10px] px-[14px] rounded-lg prevent-select text-xs md:text-sm lg:text-md"
+      className="dropdown cursor-pointer flex items-center justify-center gap-2 w-[200px] md:w-[180px] bg-white border-solid border border-gray-300 text-gray-500 py-[10px] px-[14px] rounded-lg prevent-select text-xs md:text-sm lg:text-md"
       ref={dropdownRef}
       onClick={() => {
         setDropdownOpen((open) => !open);
       }}
     >
-      <div className="grow">{newCatalogText}</div>
+      <div className="grow flex items-center justify-center">{newCatalogTextFirst} - {newCatalogTextSecond}</div>
       <div>
         {!dropdownOpen && <ChevronDown />}
         {dropdownOpen && <ChevronUp />}
@@ -48,16 +57,22 @@ const CatalogDropdown = () => {
       {dropdownOpen && (
         <div className="dropdown-choices z-10 ut-shadow-lg">
           {catalogList.map((choice) => {
+            var str = choice.text.split("-");
+            var str1 = (str[0]);
+            var str2 = (str[1]);
             return (
               <div
-                className="dropdown-choice"
+                className="grow flex items-center justify-center dropdown-choice"
                 key={choice.text}
                 data-value={choice.text}
                 onClick={() => {
                   setCatalog(choice.text);
                 }}
+                style={{ paddingLeft: '10px', paddingRight: '10px' }}
               >
-                {choice.text}
+                <span style={{ marginRight: '28px' }}>
+                  {str1} - {str2}
+                </span>
               </div>
             );
           })}

0bf7bc572023d8261400754a64bd646ee673f624
Evan Whymark <whymarkevan@gmail.com>
2024-08-06 00:35:01 -0400

    Update AppContextInterface

--- app/model/AppContextInterface.ts
+++ app/model/AppContextInterface.ts
@@ -2,9 +2,10 @@ import { IcourseStatus, IpathwayData } from "@/public/data/staticInterface";
 
 type ApplicationContextTemplate = {
   catalog_year: string;
-
+  pathwayData: any;
   // Methods
   setCatalog: (...arg: any) => any;
+  setPathways: (...arg:any) => any;
 };
 
 type ApplicationConstant = {

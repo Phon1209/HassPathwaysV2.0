e3a6248f95d38d0f8036c312e562e93be3d001ff
Evan Whymark <whymarkevan@gmail.com>
2024-08-06 00:34:32 -0400

    Updated App Context With Pathways

--- app/contexts/appContext/AppProvider.tsx
+++ app/contexts/appContext/AppProvider.tsx
@@ -8,7 +8,7 @@ import {
   ReactNode,
 } from "react";
 import { appReducer } from "./AppReducer";
-import { INITIAL_LOAD_DATA, SET_CATALOG } from "../actions";
+import {INITIAL_LOAD_DATA, SET_CATALOG, SET_PATHWAYS} from "../actions";
 import {
   courseState,
   pathwaysCategories,
@@ -19,8 +19,10 @@ import { ApplicationContext } from "@/app/model/AppContextInterface";
 const constantApplicationValue = { courseState, pathwaysCategories };
 
 const defaultInitialState: ApplicationContext = {
-  catalog_year: "2022-2023", // this value is to keep the dropdown text empty while fetching localStorage
+  catalog_year: -1, // this value is to keep the dropdown text empty while fetching localStorage
   // TODO: all course with status
+  pathwayData: "",
+  setPathways: () => {},
   setCatalog: () => {},
   ...constantApplicationValue,
 };
@@ -37,7 +39,7 @@ const AppContextProvider = ({ children }: { children: ReactNode }) => {
     const localStorageString = localStorage.getItem(APPLICATION_STATE_KEY);
     const stateWithoutLocalStorage = {
       ...defaultInitialState,
-      catalog_year: "2022-2023",
+      catalog_year: 2023,
     };
 
     dispatch({
@@ -53,14 +55,44 @@ const AppContextProvider = ({ children }: { children: ReactNode }) => {
     localStorage.setItem(APPLICATION_STATE_KEY, JSON.stringify(state));
   }, [state]);
 
+  useEffect(() => {
+    const fetchInitialData = async () => {
+      try {
+        const response = await     fetch(
+            `http://localhost:3000/api/pathway/search?${new URLSearchParams({
+              searchString: "",
+              department: "",
+              catalogYear: "2022-2023",
+            })}`);
+        const initialData = await response.json();
+        console.log("INIT: " + JSON.stringify(initialData));
+        dispatch({
+          type: SET_PATHWAYS,
+          payload: initialData,
+        });
+        console.log("INIT Pathways")
+      } catch (error) {
+        console.error("Failed to fetch initial data:", error);
+      }
+    };
+
+    fetchInitialData();
+  }, []);
+
+
   // Declare any state function here
   // and pass through value props in AppContext.Provider
-  const setCatalog = (catalog_year: string) => {
+  const setCatalog = (catalog_year: number) => {
     dispatch({ type: SET_CATALOG, payload: catalog_year });
   };
 
+
+  const setPathways = (pathwayData: any) => {
+    dispatch({type: SET_PATHWAYS, payload: pathwayData})
+  };
+
   return (
-    <AppContext.Provider value={{ ...state, setCatalog }}>
+    <AppContext.Provider value={{ ...state, setCatalog,setPathways }}>
       {children}
     </AppContext.Provider>
   );
--- app/contexts/appContext/AppReducer.tsx
+++ app/contexts/appContext/AppReducer.tsx
@@ -2,7 +2,7 @@ import {
   ApplicationContext,
   ApplicationDispatch,
 } from "@/app/model/AppContextInterface";
-import { INITIAL_LOAD_DATA, SET_CATALOG } from "../actions";
+import {INITIAL_LOAD_DATA, SET_CATALOG, SET_PATHWAYS} from "../actions";
 
 export const appReducer: (
   state: ApplicationContext,
@@ -19,6 +19,12 @@ export const appReducer: (
         ...state,
         catalog_year: action.payload,
       };
+    case SET_PATHWAYS:
+      return {
+        ...state,
+        pathwayData: action.payload,
+      }
+
     default:
       return state;
   }
